b97ae81cb7ef05ef65eae7cb134c6df1c60c50a1
Byeongsu KIM <brightdev.bs@gmail.com>
2023-12-02 18:22:51 +0900

    redis 테스트 작성

---
+++ src/test/java/com/example/bookmanagement/repository/BookCacheRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.bookmanagement.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.UUID;
+
+@SpringBootTest
+class BookCacheRepositoryTest {
+
+    @Autowired
+    BookCacheRepository bookCacheRepository;
+
+    @Autowired
+    RedisTemplate<String, String> stringRedisTemplate;
+
+    @DisplayName("레디스 데이터 저장 테스트 ")
+    @Test
+    void setTest() {
+        UUID uuid = UUID.randomUUID();
+        bookCacheRepository.setDelayedMember(uuid);
+
+        String result = stringRedisTemplate.opsForValue().get(String.valueOf(uuid));
+        Assertions.assertNotNull(result);
+    }
+
+}
