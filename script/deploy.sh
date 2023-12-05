a3d2b17b41d1c0d627d4fa452928b402910b8d11
Byeongsu KIM <brightdev.bs@gmail.com>
2023-12-05 19:19:10 +0900

    sh 로그 추가

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,18 +4,18 @@
 
 BUILD_JAR=$(ls /home/ubuntu/project/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> JAR_NAME: ${JAR_NAME}" >>
-echo "> build 파일 복사"
+echo "> JAR_NAME: ${JAR_NAME}" >> /home/ubuntu/action/deploy.log
+echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
 
 DEPLOY_PATH = /home/ubuntu/deploy
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">> 현재 실행중인 애플리케이션 PID "
+echo ">> 현재 실행중인 애플리케이션 PID " >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션 없습니다."
+  echo "> 현재 구동중인 애플리케이션 없습니다." >> /home/ubuntu/action/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -23,5 +23,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"
-nohup java -jar $DEPLOY_JAR
+echo "> DEPLOY_JAR 배포" >> /home/ubuntu/action/deploy.log
+nohup java -jar $DEPLOY_JAR  >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
