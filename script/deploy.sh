9b703e9af765ec35831b5a48886370f7e0bada08
Byeongsu KIM <brightdev.bs@gmail.com>
2023-12-05 20:13:51 +0900

    gitaction수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,18 +2,18 @@
 
 BUILD_JAR=$(ls /home/ubuntu/project/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> JAR_NAME: ${JAR_NAME}" >> /home/ubuntu/action/deploy.log
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
+echo "> JAR_NAME: ${JAR_NAME}"
+echo "> build 파일 복사"
 
 DEPLOY_PATH = /home/ubuntu/deploy
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">> 현재 실행중인 애플리케이션 PID " >> /home/ubuntu/action/deploy.log
+echo ">> 현재 실행중인 애플리케이션 PID "
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션 없습니다." >> /home/ubuntu/action/deploy.log
+  echo "> 현재 구동중인 애플리케이션 없습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,5 +21,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포" >> /home/ubuntu/action/deploy.log
+echo "> DEPLOY_JAR 배포"
 nohup java -jar $DEPLOY_JAR
